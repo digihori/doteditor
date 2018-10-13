a0badd4843e6c5acd71426505de7f131362f92a1
digihori <yo6987@gmail.com>
2018-10-13 17:35:15 +0900

    first commit

---
+++ app/src/main/java/tk/horiuchi/doteditor/MainActivity.java
@@ -0,0 +1,92 @@
+package tk.horiuchi.doteditor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static int[] digit = {0x42,0x6f,0x7f,0x7f,0x72};
+    public static float scale = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getMetrics(metrics);
+        scale = metrics.scaledDensity;
+        Log.w("Main", String.format("scaledDensity=%f\n", scale));
+
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+
+        ((TextView)findViewById(R.id.text1)).setText(String.format("%02X %02X %02X %02X %02X", digit[0], digit[1], digit[2], digit[3], digit[4]));
+    }
+
+    public void onClick(View v) {
+        int id = v.getId();
+        int temp;
+
+        switch (id) {
+            case R.id.btn1: // 左右
+                temp = digit[0];
+                digit[0] = digit[4];
+                digit[4] = temp;
+                temp = digit[1];
+                digit[1] = digit[3];
+                digit[3] = temp;
+                findViewById(R.id.myEditView).invalidate();
+                break;
+            case R.id.btn2: // 上下
+                for (int i = 0; i < 5; i++) {
+                    digit[i] = swapbit(digit[i]);
+                }
+                findViewById(R.id.myEditView).invalidate();
+                break;
+            case R.id.btn3: // 回転
+                temp = digit[0];
+                digit[0] = digit[4];
+                digit[4] = temp;
+                temp = digit[1];
+                digit[1] = digit[3];
+                digit[3] = temp;
+                for (int i = 0; i < 5; i++) {
+                    digit[i] = swapbit(digit[i]);
+                }
+                findViewById(R.id.myEditView).invalidate();
+                break;
+            case R.id.btn4: // 消去
+                for (int i = 0; i < 5; i++) {
+                    digit[i] = 0;
+                }
+                findViewById(R.id.myEditView).invalidate();
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+    private int swapbit(int x) {
+        int ans = 0;
+        ans |= (x & 1) << 6;
+        ans |= (x & 2) << 4;
+        ans |= (x & 4) << 2;
+        ans |= x & 8;
+        ans |= (x & 0x10) >> 2;
+        ans |= (x & 0x20) >> 4;
+        ans |= (x & 0x40) >> 6;
+        return ans;
+    }
+}
